de792131d3d1d66c9271bfc9aabd3b98c473eec1
vitoria-abadia <v.abadia@kingspanisoeste.com.br>
2024-04-19 17:01:02 -0300

    ⚡ perf: Relação entre Logradouro e tipo de Logradouro

--- Tarefa07/Logradouro.java
+++ Tarefa07/Logradouro.java
@@ -1,3 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Logradouro {
     private String nome; 
+    private List<TipoLogradouro> tipoLogradouros;
+
+    public Logradouro() { 
+          tipoLogradouros = new ArrayList<>();
+      }
+    
+    /**
+     * @return String return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return List<TipoLogradouro> return the tipoLogradouros
+     */
+    public List<TipoLogradouro> getTipoLogradouros() {
+        return tipoLogradouros;
+    }
+
+    /**
+     * @param tipoLogradouros the tipoLogradouros to set
+     */
+    public void setTipoLogradouros(List<TipoLogradouro> tipoLogradouros) {
+        this.tipoLogradouros = tipoLogradouros;
+    }
 }
+
