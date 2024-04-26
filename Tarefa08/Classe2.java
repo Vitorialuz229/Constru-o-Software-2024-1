7041d6fd4df2fa0d884d34a8415f179bcf1f3407
vitoria-abadia <v.abadia@kingspanisoeste.com.br>
2024-04-26 17:00:40 -0300

    Finalizando questão 2

---
+++ Tarefa08/Classe2.java
@@ -0,0 +1,9 @@
+
+public class Classe2 {
+
+    public int[] valores;
+
+    public Classe2() {
+        this.valores = new int[10];
+    }
+}
--- Tarefa08/Main2.java
+++ Tarefa08/Main2.java
@@ -8,13 +8,15 @@ public class Main2 {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-        int numero;
+        Classe2 classe2 = new Classe2();
+        int numero, i=0;
 
         try {
             do {
                 System.out.println("Digite um numero: ");
                 numero = scan.nextInt();
-                
+                classe2.valores[i] = numero;
+                i++;
             } while (numero != 0);
 
         } catch (ArrayIndexOutOfBoundsException e) {
@@ -23,6 +25,8 @@ public class Main2 {
         } catch (InputMismatchException e) {
             System.out.println("Valor informado não é numérico. ");
 
+        } finally {
+            System.out.println(Arrays.toString(classe2.valores));
         }
     }
 }
