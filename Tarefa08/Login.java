838bb49e5f06a298f7e67feb0626fbb76accaeba
vitoria-abadia <v.abadia@kingspanisoeste.com.br>
2024-04-26 17:03:58 -0300

    Login

---
+++ Tarefa08/Login.java
@@ -0,0 +1,15 @@
+public class Login {
+
+          private String usuario;
+          private String senha;
+
+          public Login(String usuario, String senha) {
+                    this.usuario = usuario;
+                    this.senha = senha;
+          }
+
+          public boolean fazerLogin(String usuario, String senha) {
+
+          }
+
+}
